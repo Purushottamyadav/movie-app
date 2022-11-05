646495d95813183125a70a1b6fefc7c09765fef8
unknown <www.purushottam9910@gmail.com>
2022-11-05 16:35:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import AddFavourites from "./components/AddFavourites";
 
 function App() {
+  const [movies,setMovies]=useState([])
+    const [searchValue,setSearchValue]=useState("")
+    const [favourites,setFavourites]=useState("")
+
+
+  const getMoviesRequest= async (searchValue)=>{
+    const url=`http://www.omdbapi.com/?s=star wars&apikey=372d8f11`;
+
+    const response= await fetch(url);
+    const responseJson=await response.json();
+
+    if(responseJson.search){
+      setMovies(responseJson.Search)
+    }
+    console.log(responseJson);
+  };
+  useEffect(()=>{
+    getMoviesRequest(searchValue);
+  },[searchValue]);
+
+  const addFavouriteMovie=(movie)=>{
+    const newFavouriteList=[...favourites,movie]
+    setFavourites(newFavouriteList)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div>hello world</div>
+   
+   <div className="container-fluid movie-app">
+    <div className="row d-flex align-items-center mt-4 mb-4" >
+      <MovieListHeading heading="Movies"/>
+      <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
     </div>
-  );
+    <div className="row">
+    <MovieList movies ={movies} handleFavouritesClick={addFavouriteMovie}favouriteComponent={AddFavourites}/> 
+    </div>
+    <div className="row d-flex align-items-center mt-4 mb-4" >
+      <MovieListHeading heading="Favourites"/>
+    </div>
+    <div className="row">
+    <MovieList movies ={movies} handleFavouritesClick={addFavouriteMovie}favouriteComponent={AddFavourites}/> 
+    </div>
+
+   </div>
+      
+  
+  )
 }
 
 export default App;
---
+++ src/components/SearchBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SearchBox = (props) => {
+    // console.log(event.target.value)
+  return (
+    <div className='col col-sm-4'>
+        <input className="form-control" value={props.value} onChange={(event)=>props.setSearchValue(event.target.value)} placeholder=' search'></input>
+      
+    </div>
+  )
+}
+
+export default SearchBox
